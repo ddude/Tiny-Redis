f04fbf25d130b6200f3d51c14f07115b4e7a3a55
Jeremie Pelletier <jeremiep@gmail.com>
2014-12-23 23:37:43 -0500

    more writeln()

--- tinyredis/connection.d
+++ tinyredis/connection.d
@@ -6,7 +6,7 @@ module tinyredis.connection;
 
 public:
     import std.socket : TcpSocket;
-	    
+
 private:
     import std.array : appender, back, popBack;
     import tinyredis.parser;
@@ -15,7 +15,7 @@ private:
 debug {
 	import std.stdio : writeln;
 	import tinyredis.encoder : escape;
-}    
+}
 
 public:
 
@@ -30,8 +30,8 @@ public:
      */
 	void send(TcpSocket conn, string encoded_cmd)
     {
-        debug { writeln("Request : '", escape(encoded_cmd) ~ "'"); }
-        
+        //debug { writeln("Request : '", escape(encoded_cmd) ~ "'"); }
+
         auto sent = conn.send(encoded_cmd);
         if (sent != (cast(byte[])encoded_cmd).length)
             throw new ConnectionException("Error while sending request");
@@ -52,19 +52,19 @@ public:
         Response[] responses;
         Response*[] MultiBulks; //Stack of pointers to multibulks
         Response[]* stackPtr = &responses;
-        
+
         while(true)
         {
             receive(conn, buffer);
-            
-            debug{ writeln("BUFFER : ", escape(cast(string)buffer)); } 
-            
+
+            //debug{ writeln("BUFFER : ", escape(cast(string)buffer)); }
+
             while(buffer.length > 0)
             {
                 auto r = parseResponse(buffer);
                 if(r.type == ResponseType.Invalid)
                      break;
-               
+
                 *stackPtr ~= r;
                 if(r.type == ResponseType.MultiBulk)
                 {
@@ -79,11 +79,11 @@ public:
                     while(MultiBulks.length > 0)
                     {
                         auto mb = *(MultiBulks.back);
-                        
+
                         if(mb.count == mb.values.length)
                         {
                             MultiBulks.popBack();
-                            
+
                             if(MultiBulks.length > 0)
                                 stackPtr = &((*MultiBulks.back).values);
                             else
@@ -93,44 +93,44 @@ public:
                             break;
                     }
             }
-            
+
             if(buffer.length == 0 && MultiBulks.length == 0) //Make sure all the multi bulks got their data
             {
-                debug {
+                /+debug {
                     if(minResponses > 1 && responses.length < minResponses)
                         writeln("WAITING FOR MORE RESPONSES ... ");
-                }
-                    
+                }+/
+
                 if(responses.length < minResponses)
                     continue;
-                    
+
                 break;
             }
-                
+
         }
-        
+
         return responses;
     }
-    
+
    /* -------- EXCEPTIONS ------------- */
- 
+
     class ConnectionException : Exception {
         this(string msg) { super(msg); }
     }
 
 
 private :
-    
+
     void receive(TcpSocket conn, ref byte[] buffer)
     {
         byte[1024 * 16] buff;
         size_t len = conn.receive(buff);
-        
+
         if(len == 0)
             throw new ConnectionException("Server closed the connection!");
         else if(len == TcpSocket.ERROR)
             throw new ConnectionException("A socket error occurred!");
 
         buffer ~= buff[0 .. len];
-        debug { writeln("Response : ", "'" ~ escape(cast(string)buff) ~ "'", " Length : ", len); }
+        //debug { writeln("Response : ", "'" ~ escape(cast(string)buff) ~ "'", " Length : ", len); }
     }
